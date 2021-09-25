028259cea999addfd5c18247b9a049235d173fa9
ainurapost <ainurapost@gmail.com>
2021-09-25 09:07:21 +0600

    adds for heroku

--- port_project/p_project/settings.py
+++ port_project/p_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
+
+django_heroku.settings(locals())
